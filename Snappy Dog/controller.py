cc816852f58091e732ffc67ad4060180a12a4775
Gabriel da Silva Godinho <108307888+GSGodinho@users.noreply.github.com>
2023-10-04 13:02:17 -0300

    Add files via upload

--- Snappy Dog/controller.py
+++ Snappy Dog/controller.py
@@ -1,5 +1,7 @@
+import tkinter as TK
+import tkinter.ttk as TKK
 from model import *
-def cadastrar_cliente(entrada_Nome,entrada_Telefone,entrada_Endereco,entrada_Cpf):
+def cadastrar_cliente(entrada_Nome,entrada_Telefone,entrada_Cpf,entrada_Endereco):
     """if Nome == '':
         return 0
     elif telefone == '' or len(telefone)!= 9 :
@@ -9,15 +11,21 @@ def cadastrar_cliente(entrada_Nome,entrada_Telefone,entrada_Endereco,entrada_Cpf
     else:
         Cliente=Cliente.create(Nome,telefone,cpf,endereco)
         return 1"""
+    
 
     Cliente=clientes.create(nome=entrada_Nome,telefone=entrada_Telefone,cpf=entrada_Cpf,endereco=entrada_Endereco)
+
 
 
 def cadastrar_servico(Entrada_Nome,Entrada_Descricao,Entrada_Preco):
     servico=servicos.create(nome=Entrada_Nome,descricao=Entrada_Descricao,preco=Entrada_Preco)
 
-# #def Cadastrar_Animal():
-#     Animal=animais.create
+def Cadastrar_Animal(Entrada_Nome,Entrada_Especie,Entrada_Raca,Entrada_Porte,Entrada_cpf_dono):
+     
+     Animal=animais.create()
+
+
+
 
 
     
--- Snappy Dog/view.py
+++ Snappy Dog/view.py
@@ -1,20 +1,19 @@
-from tkinter import *
-from tkinter.ttk import *
+
 
 from controller import *
 
-tela=Tk()
+tela=TK.Tk()
 tela.geometry('800x450')
 tela.title('Petshop')
 
-api=Notebook(tela)
+api=TKK.Notebook(tela)
 api.pack(expand=True,fill='both')
 api.option_add('*Font',"Arial 12")
 
-Cadastro=Notebook(api)
+Cadastro=TKK.Notebook(api)
 Cadastro.pack(expand=True,fill='both')
 
-consulta=Notebook(api)
+consulta=TKK.Notebook(api)
 consulta.pack(expand=True,fill='both')
 
 api.add(Cadastro,text='Cadastro')
@@ -22,114 +21,104 @@ api.add(consulta,text='Consulta')
 
 
 #Criação das Telas
-cadastro_cliente=Frame(Cadastro)
-cadastro_animal=Frame(Cadastro)
-cadastro_servico=Frame(Cadastro)
-#Funções para Cadastros
-def Enviar_Cliente():
-    #enviar as Informações ao controlador:
-    cadastrar_cliente(entry_cliente_nome.get(),
+cadastro_cliente=TK.Frame(Cadastro)
+cadastro_animal=TK.Frame(Cadastro)
+cadastro_servico=TK.Frame(Cadastro)
+
+def LimparFrame(telapai):
+    for widget in telapai.winfo_children():
+        if isinstance(widget,TK.Entry):
+            widget.delete(0, "end")
+        if isinstance(widget,TK.Text):
+            widget.delete(1.0, "end")
+
+def Enviar_Cadastro_Cliente(Tela,Nome,telefone,cpf,endereco):
+    cadastrar_cliente(Nome,telefone,cpf,endereco)
+    LimparFrame(Tela)
+# def Enviar_Cadastro_animal():
+#     cadastro_animal()
+
+def Enviar_Cadastro_Servico(Tela,nome,descricao,preco):
+    cadastrar_servico(nome,descricao,preco)
+    LimparFrame(Tela)
+
+
+#cadrasto Cliente
+label_cliente_nome=TK.Label(cadastro_cliente,text='Nome').grid(column=0,row=0)
+
+entry_cliente_nome=TK.Entry(cadastro_cliente)
+entry_cliente_nome.grid(column=1,row=0,pady=10)
+
+label_cliente_telefone=TK.Label(cadastro_cliente,text='Telefone').grid(column=0,row=1)
+
+entry_cliente_telefone=TK.Entry(cadastro_cliente)
+entry_cliente_telefone.grid(column=1,row=1,pady=10)
+
+label_cliente_cpf=TK.Label(cadastro_cliente,text='CPF').grid(column=0,row=2)
+
+entry_cliente_cpf=TK.Entry(cadastro_cliente)
+entry_cliente_cpf.grid(column=1,row=2,pady=10)
+
+label_cliente_endereco=TK.Label(cadastro_cliente,text='Endereço').grid(column=0,row=3)
+
+entry_cliente_endereco=TK.Entry(cadastro_cliente)
+entry_cliente_endereco.grid(column=1,row=3,pady=10)
+
+button_cliente_enviar=TK.Button(cadastro_cliente,text='Enviar',command=lambda: Enviar_Cadastro_Cliente(cadastro_cliente,entry_cliente_nome.get(),
                       entry_cliente_telefone.get(),
                       entry_cliente_endereco.get(),
                       entry_cliente_cpf.get())
-    #apaga
-    # entry_cliente_nome.delete(0, 'end')
-    # entry_cliente_telefone.delete(0, 'end')
-    # entry_cliente_endereco.delete(0, 'end')
-    # entry_cliente_cpf.delete(0, 'end')
-
-# def enviar_Animal():
-#     Cadastrar_Animal(entry_animal_nome.get(),
-#                      entry_animal_raca(),
-#                      entry_animal_dono_cpf.get(),
-#                      commobox_tipo_animal.get())
-def Enviar_Servico():
-    cadastrar_servico(entry_servico_nome.get(),
-                      text_servico_descricao.get(1.0, "end-1c"),
-                      entry_servico_preco.get())
-    
-
-#cadrasto Cliente
-label_cliente_nome=Label(cadastro_cliente,text='nome')
-label_cliente_nome.grid(column=0,row=0)
-
-entry_cliente_nome=Entry(cadastro_cliente)
-entry_cliente_nome.grid(column=1,row=0)
-
-label_cliente_telefone=Label(cadastro_cliente,text='Telefone')
-label_cliente_telefone.grid(column=0,row=1)
-
-entry_cliente_telefone=Entry(cadastro_cliente)
-entry_cliente_telefone.grid(column=1,row=1)
-
-label_cliente_cpf=Label(cadastro_cliente,text='CPF')
-label_cliente_cpf.grid(column=0,row=2)
-
-entry_cliente_cpf=Entry(cadastro_cliente)
-entry_cliente_cpf.grid(column=1,row=2)
-
-label_cliente_endereco=Label(cadastro_cliente,text='Endereço')
-label_cliente_endereco.grid(column=0,row=3)
-
-entry_cliente_endereco=Entry(cadastro_cliente)
-entry_cliente_endereco.grid(column=1,row=3)
-
-button_cliente_enviar=Button(cadastro_cliente,text='Enviar',command= Enviar_Cliente())
-button_cliente_enviar.grid(column=0,columnspan=2,row=4)
+                      ).grid(column=0,columnspan=2,row=4,pady=10)
 
 #cadrasto Animal
-label_animal_nome=Label(cadastro_animal,text='nome do animal',)
-label_animal_nome.grid(column=0,row=0)
+label_animal_nome=TK.Label(cadastro_animal,text='nome do animal',).grid(column=0,row=0)
 
-entry_animal_nome=Entry(cadastro_animal)
+entry_animal_nome=TK.Entry(cadastro_animal)
 entry_animal_nome.grid(column=1,row=0)
 
-label_animal_raca=Label(cadastro_animal,text='raça')
-label_animal_raca.grid(column=0,row=1)
+label_animal_raca=TK.Label(cadastro_animal,text='raça').grid(column=0,row=1)
 
-entry_animal_raca=Entry(cadastro_animal)
+entry_animal_raca=TK.Entry(cadastro_animal)
 entry_animal_raca.grid(column=1,row=1)
 
-label_animal_dono_cpf=Label(cadastro_animal,text='cpf do dono')
-label_animal_dono_cpf.grid(column=0,row=2)
+label_animal_dono_cpf=TK.Label(cadastro_animal,text='cpf do dono').grid(column=0,row=2)
 
-entry_animal_dono_cpf=Entry(cadastro_animal)
+entry_animal_dono_cpf=TK.Entry(cadastro_animal)
 entry_animal_dono_cpf.grid(column=1,row=2)
 
-label_animal_raca=Label(cadastro_animal,text='Tipo de animal')
-label_animal_raca.grid(column=0,row=3)
+label_animal_raca=TK.Label(cadastro_animal,text='Tipo de animal').grid(column=0,row=3)
 
-tipo_animal=StringVar()
-commobox_tipo_animal=Combobox(cadastro_animal,textvariable=tipo_animal)
+tipo_animal=TK.StringVar()
+commobox_tipo_animal=TKK.Combobox(cadastro_animal,textvariable=tipo_animal)
 commobox_tipo_animal['value']= ['Gato','Cachorro','outros']
 commobox_tipo_animal.grid(column=1,row=3)
 
-button_animal_enviar=Button(cadastro_animal,text='Enviar')
+button_animal_enviar=TK.Button(cadastro_animal,text='Enviar')
 button_animal_enviar.grid(column=0,columnspan=2,row=4)
 
 #Cadastro serviço
 
-label_servico_nome=Label(cadastro_servico,text='nome do serviço')
-label_servico_nome.grid(column=0,row=0)
+label_servico_nome=TK.Label(cadastro_servico,text='nome do serviço').grid(column=0,row=0)
 
-entry_servico_nome=Entry(cadastro_servico)
+entry_servico_nome=TK.Entry(cadastro_servico)
 entry_servico_nome.grid(column=1,row=0)
 
-label_servico_descricao=Label(cadastro_servico,text='Descricao')
-label_servico_descricao.grid(column=0,row=1)
+label_servico_descricao=TK.Label(cadastro_servico,text='Descricao').grid(column=0,row=1)
 
-text_servico_descricao=Text(cadastro_servico,width=20,height=5)
+text_servico_descricao=TK.Text(cadastro_servico,width=20,height=5)
 text_servico_descricao.grid(column=1,row=1,pady=10)
 
-label_servico_preco=Label(cadastro_servico,text='Preço do serviço')
-label_servico_preco.grid(column=0,row=2)
+label_servico_preco=TK.Label(cadastro_servico,text='Preço do serviço').grid(column=0,row=2)
 
-entry_servico_preco=Entry(cadastro_servico)
+entry_servico_preco=TK.Entry(cadastro_servico)
 entry_servico_preco.grid(column=1,row=2)
 
-button_servico_enviar=Button(cadastro_servico,text='Enviar',command=Enviar_Servico())
-button_animal_enviar.grid(column=0,columnspan=2,row=4)
-button_servico_enviar.grid(column=0,columnspan=2,row=4)
+button_servico_enviar=TK.Button(cadastro_servico,text='Enviar',command= 
+                                lambda:Enviar_Cadastro_Servico(cadastro_servico,
+                                                                entry_servico_nome.get(),
+                                                                text_servico_descricao.get("1.0",'end-1c'),
+                                                                entry_servico_preco.get()
+                                                                )).grid(column=0,columnspan=2,row=4)
 
 #link dos frames para os notebook
 Cadastro.add(cadastro_cliente,text='Cadastro Cliente')
